a9b017e59a85a90f6a1dddbff9df4d0b8fde42c5
Natalie Chen <v_natali@mediatek.com>
2022-12-19 17:03:31 +0800

    fix new window issue

--- workbench.ts
+++ workbench.ts
@@ -1,10 +1,202 @@
-import {
-  create
-} from "vs/workbench/workbench.web.main";
-import { URI, UriComponents } from "vs/base/common/uri";
-import { IWorkbenchConstructionOptions } from "vs/workbench/browser/web.api";
-import { IWorkspace, IWorkspaceProvider } from "vs/workbench/services/host/browser/browserHostService";
+import { isStandalone } from 'vs/base/browser/browser';
+import { parse } from 'vs/base/common/marshalling';
+import { Schemas } from 'vs/base/common/network';
+import { isEqual } from 'vs/base/common/resources';
+import { URI, UriComponents } from 'vs/base/common/uri';
+import { isFolderToOpen, isWorkspaceToOpen } from 'vs/platform/window/common/window';
+import { create } from 'vs/workbench/workbench.web.main';
+import { posix } from 'vs/base/common/path';
+import { ltrim } from 'vs/base/common/strings';
+import type { IWorkbenchConstructionOptions } from 'vs/workbench/browser/web.api';
+import type { IWorkspace, IWorkspaceProvider } from 'vs/workbench/services/host/browser/browserHostService';
+
 declare const window: any;
+
+class WorkspaceProvider implements IWorkspaceProvider {
+
+	private static QUERY_PARAM_EMPTY_WINDOW = 'ew';
+	private static QUERY_PARAM_FOLDER = 'folder';
+	private static QUERY_PARAM_WORKSPACE = 'workspace';
+
+	private static QUERY_PARAM_PAYLOAD = 'payload';
+
+	static create(config: IWorkbenchConstructionOptions & { folderUri?: UriComponents; workspaceUri?: UriComponents }) {
+		let foundWorkspace = false;
+		let workspace: IWorkspace;
+		let payload = Object.create(null);
+
+		const query = new URL(document.location.href).searchParams;
+		query.forEach((value, key) => {
+			switch (key) {
+
+				// Folder
+				case WorkspaceProvider.QUERY_PARAM_FOLDER:
+					if (config.remoteAuthority && value.startsWith(posix.sep)) {
+						// when connected to a remote and having a value
+						// that is a path (begins with a `/`), assume this
+						// is a vscode-remote resource as simplified URL.
+						workspace = { folderUri: URI.from({ scheme: Schemas.vscodeRemote, path: value, authority: config.remoteAuthority }) };
+					} else {
+						workspace = { folderUri: URI.parse(value) };
+					}
+					foundWorkspace = true;
+					break;
+
+				// Workspace
+				case WorkspaceProvider.QUERY_PARAM_WORKSPACE:
+					if (config.remoteAuthority && value.startsWith(posix.sep)) {
+						// when connected to a remote and having a value
+						// that is a path (begins with a `/`), assume this
+						// is a vscode-remote resource as simplified URL.
+						workspace = { workspaceUri: URI.from({ scheme: Schemas.vscodeRemote, path: value, authority: config.remoteAuthority }) };
+					} else {
+						workspace = { workspaceUri: URI.parse(value) };
+					}
+					foundWorkspace = true;
+					break;
+
+				// Empty
+				case WorkspaceProvider.QUERY_PARAM_EMPTY_WINDOW:
+					workspace = undefined;
+					foundWorkspace = true;
+					break;
+
+				// Payload
+				case WorkspaceProvider.QUERY_PARAM_PAYLOAD:
+					try {
+						payload = parse(value); // use marshalling#parse() to revive potential URIs
+					} catch (error) {
+						console.error(error); // possible invalid JSON
+					}
+					break;
+			}
+		});
+
+		// If no workspace is provided through the URL, check for config
+		// attribute from server
+		if (!foundWorkspace) {
+			if (config.folderUri) {
+				workspace = { folderUri: URI.revive(config.folderUri) };
+			} else if (config.workspaceUri) {
+				workspace = { workspaceUri: URI.revive(config.workspaceUri) };
+			}
+		}
+
+		return new WorkspaceProvider(workspace, payload, config);
+	}
+
+	readonly trusted = true;
+
+	private constructor(
+		readonly workspace: IWorkspace,
+		readonly payload: object,
+		private readonly config: IWorkbenchConstructionOptions
+	) {
+	}
+
+	async open(workspace: IWorkspace, options?: { reuse?: boolean; payload?: object }): Promise<boolean> {
+		if (options?.reuse && !options.payload && this.isSame(this.workspace, workspace)) {
+			return true; // return early if workspace and environment is not changing and we are reusing window
+		}
+
+		const targetHref = this.createTargetUrl(workspace, options);
+		if (targetHref) {
+			if (options?.reuse) {
+				window.location.href = targetHref;
+				return true;
+			} else {
+				let result;
+				if (isStandalone()) {
+					result = window.open(targetHref, '_blank', 'toolbar=no'); // ensures to open another 'standalone' window!
+				} else {
+					result = window.open(targetHref);
+				}
+
+				return !!result;
+			}
+		}
+		return false;
+	}
+
+	private createTargetUrl(workspace: IWorkspace, options?: { reuse?: boolean; payload?: object }): string | undefined {
+
+		// Empty
+		let targetHref: string | undefined = undefined;
+		if (!workspace) {
+			targetHref = `${document.location.origin}${document.location.pathname}?${WorkspaceProvider.QUERY_PARAM_EMPTY_WINDOW}=true`;
+		}
+
+		// Folder
+		else if (isFolderToOpen(workspace)) {
+			let queryParamFolder: string;
+			if (this.config.remoteAuthority && workspace.folderUri.scheme === Schemas.vscodeRemote) {
+				// when connected to a remote and having a folder
+				// for that remote, only use the path as query
+				// value to form shorter, nicer URLs.
+				// ensure paths are absolute (begin with `/`)
+				// clipboard: ltrim(workspace.folderUri.path, posix.sep)
+				queryParamFolder = `${posix.sep}${ltrim(workspace.folderUri.path, posix.sep)}`;
+			} else {
+				queryParamFolder = encodeURIComponent(workspace.folderUri.toString(true));
+			}
+
+			targetHref = `${document.location.origin}${document.location.pathname}?${WorkspaceProvider.QUERY_PARAM_FOLDER}=${queryParamFolder}`;
+		}
+
+		// Workspace
+		else if (isWorkspaceToOpen(workspace)) {
+			let queryParamWorkspace: string;
+			if (this.config.remoteAuthority && workspace.workspaceUri.scheme === Schemas.vscodeRemote) {
+				// when connected to a remote and having a workspace
+				// for that remote, only use the path as query
+				// value to form shorter, nicer URLs.
+				// ensure paths are absolute (begin with `/`)
+				queryParamWorkspace = `${posix.sep}${ltrim(workspace.workspaceUri.path, posix.sep)}`;
+			} else {
+				queryParamWorkspace = encodeURIComponent(workspace.workspaceUri.toString(true));
+			}
+
+			targetHref = `${document.location.origin}${document.location.pathname}?${WorkspaceProvider.QUERY_PARAM_WORKSPACE}=${queryParamWorkspace}`;
+		}
+
+		// Append payload if any
+		if (options?.payload) {
+			targetHref += `&${WorkspaceProvider.QUERY_PARAM_PAYLOAD}=${encodeURIComponent(JSON.stringify(options.payload))}`;
+		}
+
+		return targetHref;
+	}
+
+	private isSame(workspaceA: IWorkspace, workspaceB: IWorkspace): boolean {
+		if (!workspaceA || !workspaceB) {
+			return workspaceA === workspaceB; // both empty
+		}
+
+		if (isFolderToOpen(workspaceA) && isFolderToOpen(workspaceB)) {
+			return isEqual(workspaceA.folderUri, workspaceB.folderUri); // same workspace
+		}
+
+		if (isWorkspaceToOpen(workspaceA) && isWorkspaceToOpen(workspaceB)) {
+			return isEqual(workspaceA.workspaceUri, workspaceB.workspaceUri); // same workspace
+		}
+
+		return false;
+	}
+
+	hasRemote(): boolean {
+		if (this.workspace) {
+			if (isFolderToOpen(this.workspace)) {
+				return this.workspace.folderUri.scheme === Schemas.vscodeRemote;
+			}
+
+			if (isWorkspaceToOpen(this.workspace)) {
+				return this.workspace.workspaceUri.scheme === Schemas.vscodeRemote;
+			}
+		}
+
+		return true;
+	}
+}
 
 (async function () {
   // create workbench
@@ -38,17 +230,8 @@ declare const window: any;
     workspace = undefined;
   }
 
-  if (workspace) {
-    const workspaceProvider: IWorkspaceProvider = {
-      workspace,
-      open: async (
-        workspace: IWorkspace,
-        options?: { reuse?: boolean; payload?: object }
-      ) => true,
-      trusted: true,
-    };
-    config = { ...config, workspaceProvider };
-  }
+  const workspaceProvider: IWorkspaceProvider = WorkspaceProvider.create(config);
+  config = { ...config, workspaceProvider };
 
   const domElement = !!config.domElementId
     && document.getElementById(config.domElementId)
